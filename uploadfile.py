ec025cff8bb01e051aeaba94ed7cd5e4d0616ad7
Jaideep Singh <jaideep.singh@morningstar.com>
2014-05-16 16:33:48 -0500

    Updated code with async, separated html/xml and updated README

--- uploadfile.py
+++ uploadfile.py
@@ -1,90 +1,69 @@
-import tornado.ioloop
-import tornado.web
-import tornado.gen
-import hashlib
-
+from tornado import ioloop, web, gen
+from hashlib import sha224
+from nltk import word_tokenize
 '''
 TODO:
 -refactor
--check for size
--async + correct HTTP REST responses
--count words better
+-correct HTTP REST responses like localhost:8888/uploaded/HASH?mimetype=application/xml
 -application design, test coverage and readability
 -Test coverage needs success and failure conditions
 -README
 '''
 
 filestatsdict = {}
+    
+class FormHandler(web.RequestHandler):
+    def get(self, ):
+        self.render('index.html')
+  
+    @gen.coroutine
+    def post(self, ):
+        global filestatsdict
+        inputfile = self.request.files['fileupload'][0] #collect the file
+        filetype = inputfile['content_type']
+        filesize = len(inputfile['body'])
+        if filesize > 10485760: #check for 10 MB
+            self.write('<html><h2>File is larger than 10 MB</h2></html>')
+            self.finish()            
+        if filetype != 'text/plain':
+            self.write('<html><h2>File type not supported</h2></html>')
+            self.finish()
+        hashed = sha224(inputfile['body']).hexdigest() #find hash.. should I use uuid?
+        status = 'Exists' #default
+        if hashed not in filestatsdict.keys():
+            status='New' #new file!
+            yield self._calcStats(inputfile=inputfile, hashed=hashed) #calcStats
+            yield self._write(outputfile=inputfile) #write file
+        numberofwords = filestatsdict[hashed]
+        self.set_header("Content-Type", "text/xml")
+        self.render('uploaded.xml', status=status, filename=inputfile['filename'], numberofwords=str(len(numberofwords)), words=numberofwords)
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self, ):
-        self.write('Hello world')
-    
-class MyFormHandler(tornado.web.RequestHandler):
-    def get(self, ):
-        self.write('<html><body><form action="/myform/" enctype="multipart/form-data" method="post">'
-                   '<input type="text" name="message"><br>'
-                   '<input type="file" name="fileupload" accept="text/plain">'
-                   '<input type="submit" value="Submit">'
-                   '</form></body></html>')
-        
-    def _calcHash(self, filez):
-        print 'in _calcHash'
-        return hashlib.sha224(filez).hexdigest()
-    
-    @tornado.gen.coroutine
-    def _calcStats(self, filez):
-    #The number of words in the file.
-    #The occurrences of each word, as some words may have been repeated.
-        print 'in _calcStats'
+    @gen.coroutine
+    def _calcStats(self, inputfile=None, hashed=None):
         global filestatsdict
         worddict = {}
-        hashed = self._calcHash(filez['body'])
-        if hashed not in filestatsdict.keys():
-            for word in filez['body'].split():
-                if word in worddict.iterkeys():
-                    worddict[word] += 1
-                else:
-                    worddict[word] = 1
+        sentence = word_tokenize(inputfile['body'])
+        for word in sentence:
+            word = ''.join(e for e in word if e.isalnum()) #removing anything thats not alphanumeric
+            if word == '':
+                pass
+            elif word in worddict.iterkeys():
+                worddict[word] += 1
+            else:
+                worddict[word] = 1
         filestatsdict[hashed] = worddict
-        return
-    
-    def _write(self, filez=None):
-        print 'in _write'
-        fileio = open('uploads/'+filez['filename'], 'w')
-        fileio.write(filez['body'])
+
+    @gen.coroutine
+    def _write(self, outputfile=None):
+        fileio = open('uploads/'+outputfile['filename'], 'w')
+        fileio.write(outputfile['body'])
         fileio.close()
-    
-    #@tornado.web.asynchronous
-    @tornado.gen.coroutine
-    def post(self, ):
-        print 'in post'
-        inputfile = self.request.files['fileupload'][0]
-        filename = inputfile['filename']
-        
-        response  = yield self._calcStats(filez=inputfile)
-        print 'returning in post'
-        print response
-        hashish = filestatsdict[self._calcHash(inputfile['body'])]
-        self._write(inputfile)
-        
-        self.set_header("Content-Type", "text/xml")
-        self.write('<?xml version="1.0" encoding="UTF-8"?>'
-                   '<response status="OK"><filename>'+filename+'</filename>'
-                   '<NumberOfWords>'+str(len(hashish))+'</NumberOfWords>'
-                   '<words>Fill this</words></response>')
-        self.finish()
-            
 
-
-application = tornado.web.Application([
-    (r"/", MainHandler),
-    (r"/myform/", MyFormHandler),
-    (r"/upload", MyFormHandler)
-])
-
+application = web.Application([
+    (r"/", FormHandler),
+    (r"/uploaded/", FormHandler),
+], debug=True)
 
 if __name__ == '__main__':
-    print 'Listening at localhost:8888'
     application.listen(8888)
-    tornado.ioloop.IOLoop.instance().start()
+    ioloop.IOLoop.instance().start()
